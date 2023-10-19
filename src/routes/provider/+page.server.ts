b1167e72ed2be0943a4a8ef4ee290e09728fa91b
Thabang Sono <tsono1@csir.co.za>
2023-10-19 15:17:11 +0200

    WIP: create provider form validation

--- src/routes/provider/+page.server.ts
+++ src/routes/provider/+page.server.ts
@@ -1,5 +1,5 @@
 import { prisma } from '$lib';
-import { error } from '@sveltejs/kit';
+import { error, fail } from '@sveltejs/kit';
 import { z } from 'zod';
 import { zfd } from 'zod-form-data';
 
@@ -12,9 +12,16 @@ export async function load() {
 }
 
 const schema = zfd.formData({
-	name: zfd.text(),
-	description: zfd.text(),
-	logo_uri: zfd.text(z.string().url()),
+	name: z
+		.string()
+		.min(2, { message: 'Two or more characters are required' })
+		.max(25, { message: 'Name should have 25 or less characters' })
+		.trim(),
+	description: z
+		.string()
+		.min(10, { message: 'Description should have 10 or more characters' })
+		.max(50, { message: '50 or less characters are required' }),
+	logo_uri: z.string().url({ message: 'Enter a valid logo URL' }),
 	isActive: zfd.checkbox()
 });
 
@@ -25,9 +32,7 @@ export const actions = {
 		const parsedForm = schema.safeParse(form);
 
 		if (!parsedForm.success) {
-			throw error(404, {
-				message: parsedForm.error.message
-			});
+			return fail(400, { error: parsedForm.error.flatten().fieldErrors });
 		}
 
 		const { name, description, logo_uri, isActive } = parsedForm.data;
@@ -46,29 +51,25 @@ export const actions = {
 		}
 	},
 
-
 	async delete({ request }) {
 		const form = await request.formData();
 
 		const schema = zfd.formData({
-			provider_id: zfd.numeric(),
-		})
+			provider_id: zfd.numeric()
+		});
 
 		const parsedForm = schema.safeParse(form);
-		
 
-
-		if (!parsedForm.success){
+		if (!parsedForm.success) {
 			throw error(404, {
 				message: parsedForm.error.message
 			});
 		}
 
 		const { provider_id } = parsedForm.data;
-		 
+
 		await prisma.provider.delete({
 			where: { id: provider_id }
 		});
-		
 	}
-}
+};
--- src/routes/provider/+page.svelte
+++ src/routes/provider/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 
 	export let data;
 
+	export let form;
+
+	export let errors = form?.error;
+	console.log(form?.error);
 </script>
 
 <h1>Provider</h1>
@@ -9,14 +14,23 @@
 <form method="POST" action="?/create">
 	<label>
 		Name
+		{#if errors?.name}
+			<label>{errors?.name[0]}</label>
+		{/if}
 		<input name="name" />
 	</label>
 	<label>
 		Description
+		{#if errors?.description}
+			<label>{errors?.description[0]}</label>
+		{/if}
 		<input name="description" />
 	</label>
 	<label>
 		Logo URI
+		{#if errors?.logo_uri}
+			<label>{errors?.logo_uri[0]}</label>
+		{/if}
 		<input name="logo_uri" />
 	</label>
 	<label>
@@ -36,7 +50,7 @@
 					<a href="/provider/{provider.id}">{provider.name}</a>
 
 					<form method="POST" action="?/delete">
-					<button name="provider_id" value={provider.id}>Delete</button>
+						<button name="provider_id" value={provider.id}>Delete</button>
 					</form>
 				</nav>
 			</li>
